611ffef24862b6d5734a1ad980cbefc10bbc2fbf
luiza <luiza.rubim@sou.inteli.edu.br>
2024-05-02 00:26:39 -0300

    código inicial movimentação

--- workspace/src/ponderada_sem2/ponderada_sem2/desenhando.py
+++ workspace/src/ponderada_sem2/ponderada_sem2/desenhando.py
@@ -14,4 +14,32 @@ class TurtleDraw(Node):
         )
 
         timer_period=0.5
-        
+        self.timer=self.create_timer(
+            timer_period_sec=timer_period,
+            callback=self.timer_callback
+        )
+
+    def timer_callback(self):
+        msg=Twist()
+        msg.linear.x=1.0
+        msg.angular.z=3.0
+        self.publisher.publish(msg)
+        self.get_logger().info("publicando velocidades para a tartaruga")
+    
+    def move_turtle(self):
+        msg=Twist()
+        msg.linear.x=1.0
+        msg.angular.z=3.0
+        self.publisher.publish(msg)
+        self.get_logger().info("publicando velocidades para a tartaruga")
+
+def main(args=None):
+    rclpy.init(args=args)
+    td = TurtleDraw()
+    rclpy.spin(td)
+    td.destroy_node() 
+    rclpy.shutdown()
+
+if __name__ =="__main__":
+    main()
+
